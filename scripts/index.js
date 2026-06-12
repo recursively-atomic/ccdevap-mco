62c3d1c371e324b389a0adada7df441a442bfd0c
Atomic <anthony.namol.090105@gmail.com>
2026-06-12 22:20:34 +0800

    feat: Add passenger details tab in profile.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,55 @@
-$(document).ready(function () {
+$(function () {
+    autocompleteLocations();
+});
+
+/**
+ * Provides autocomplete to the origin and destination text input
+ * in the flight search quick widget.
+ */
+function autocompleteLocations() {
+    const $data = $('#result');
+    
+    // Sample data of airports
     const locations = [
-        { name: "Bacolod", code: "BCD" },
-        { name: "Cagayan de Oro", code: "CGY" },
-        { name: "Davao", code: "DVO" },
-        { name: "El Nido", code: "ENI" },
-        { name: "General Santos", code: "GES" },
-        { name: "Iloilo", code: "ILO" },
-        { name: "Kalibo", code: "KLO" },
-        { name: "Laoag", code: "LAO" },
-        { name: "Manila", code: "MNL" },
-        { name: "Naga", code: "WNP" },
-        { name: "Ozamiz", code: "OZC" },
-        { name: "Pagadian", code: "PAG" },
-        { name: "Roxas", code: "RXS" },
-        { name: "San Jose, Mindoro", code: "SJI" },
-        { name: "Tacloban", code: "TAC" },
-        { name: "Virac", code: "VRC" },
-        { name: "Zamboanga", code: "ZAM" }
+        { name: 'Bacolod', code: 'BCD' },
+        { name: 'Cagayan de Oro', code: 'CGY' },
+        { name: 'Davao', code: 'DVO' },
+        { name: 'El Nido', code: 'ENI' },
+        { name: 'General Santos', code: 'GES' },
+        { name: 'Iloilo', code: 'ILO' },
+        { name: 'Kalibo', code: 'KLO' },
+        { name: 'Laoag', code: 'LAO' },
+        { name: 'Manila', code: 'MNL' },
+        { name: 'Naga', code: 'WNP' },
+        { name: 'Ozamiz', code: 'OZC' },
+        { name: 'Pagadian', code: 'PAG' },
+        { name: 'Roxas', code: 'RXS' },
+        { name: 'San Jose, Mindoro', code: 'SJI' },
+        { name: 'Tacloban', code: 'TAC' },
+        { name: 'Virac', code: 'VRC' },
+        { name: 'Zamboanga', code: 'ZAM' }
     ];
 
-    const $data = $('#result');
+    // Populating the datalist with all the sample data
     locations.forEach(location => {
         const $option = $('<option>').val(location.name).attr('data-code', location.code);
 
         $data.append($option);
     });
 
+    // Updating the datalist when a user inputs something
     $('#origin, #destination').on('change', function () {
         const $input = $(this);
         const value = $input.val();
+        let $match, code, location;
 
-        const $match = $data.find('option').filter(function () {
+        $match = $data.find('option').filter(function () {
             return $(this).val() === value;
         }).first();
 
-        const code = $match.attr('data-code');
-        const location = $match.attr('value');
+        code = $match.attr('data-code');
+        location = $match.attr('value');
 
         $input.val(location + " " + code);
     });
-});
+}
---
+++ scripts/profile.js
@@ -0,0 +1,78 @@
+$(function () {
+    showAgeBadge();
+    showSpecifyGender();
+});
+
+/**
+ * Shows a badge on what age group the passenger is based on
+ * the birthdate inputted.
+ */
+function showAgeBadge() {
+    const $dateContainer = $('#date-container');
+    const $dateInput = $('#date-input');
+    const $ageBadgeContainer = $('#age-badge-container');
+    const $ageBadge = $('#age-badge');
+
+    $dateInput.on('change', function () {
+        const inputValue = $dateInput.val();
+
+        // If there is a date input, show the badge
+        if (inputValue) {
+            const currentDate = new Date();
+            const birthDate = new Date(inputValue);
+            const differenceInDays = Math.floor((currentDate - birthDate) / (1000 * 60 * 60 * 24));
+            let ageCategories;
+            let ageCategory;
+            let ageBadgeText;
+
+            ageCategories = [
+                { label: 'Infant', min: 0, max: 730 },
+                { label: 'Child', min: 731, max: 6570 },
+                { label: 'Adult', min: 6571, max: 21900 },
+                { label: 'Senior', min: 21901, max: Infinity }
+            ];
+
+            ageCategory = ageCategories.find(category =>
+                differenceInDays >= category.min && differenceInDays <= category.max);
+            ageBadgeText = ageCategory ? ageCategory.label : '';
+
+            // If the date input is before or on the current date, show the badge
+            if (ageBadgeText) {
+                $dateContainer.removeClass('col-12').addClass('col-11');
+                $dateInput.removeClass('is-invalid').addClass('is-valid');
+
+                $ageBadgeContainer.removeClass('d-none').addClass('col-1');
+                $ageBadge.text(ageBadgeText);
+            } else {
+                $dateContainer.removeClass('col-11').addClass('col-12');
+                $dateInput.removeClass('is-valid').addClass('is-invalid');
+
+                $ageBadgeContainer.removeClass('col-1').addClass('d-none');
+            }
+        } else {
+            $dateContainer.removeClass('col-11').addClass('col-12');
+            $dateInput.removeClass('is-valid').removeClass('is-invalid');
+
+            $ageBadgeContainer.removeClass('col-1').addClass('d-none');
+        }
+    });
+}
+
+/**
+ * Shows an "Please Specify" text field when the user selects "Not Listed".
+ */
+function showSpecifyGender() {
+    const $genderSelect = $('#gender-select');
+    const $otherGenderContainer = $('#other-gender-container');
+    const $otherGenderInput = $('#other-gender-input');
+
+    $genderSelect.on('change', function () {
+        if ($genderSelect.val() === 'Not Listed') {
+            $otherGenderContainer.removeClass('d-none');
+            $otherGenderInput.focus();
+        } else {
+            $otherGenderContainer.addClass('d-none');
+            $otherGenderInput.val('');
+        }
+    });
+}
