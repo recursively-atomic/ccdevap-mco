67221b369cf05e1ecd5de778a3c446a253b9508a
Atomic <anthony.namol.090105@gmail.com>
2026-06-18 22:47:14 +0800

    style: Clean up UI

---
+++ scripts/search.js
@@ -0,0 +1,16 @@
+(function () {
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
