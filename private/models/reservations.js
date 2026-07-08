5a0efedf96f600eac6700e1d785103df928e461d
Atomic <anthony.namol.090105@gmail.com>
2026-07-08 23:09:24 +0800

    docs: Change schema field names

--- private/models/reservations.js
+++ private/models/reservations.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    // reservation-number (BR####)
-    // flight-number - refereces the flights collection
-    // (first-name + last-name) passenger-name - references the users collection
-    // seat-number
+    // reservationNumber (BR####)
+    // flightNumber - refereces the flights collection
+    // (firstName + lastName) passenger-name - references the users collection
+    // seatNumber
     // status (Paid, Pending, Cancelled, emerut)
 });
 
