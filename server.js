96293b78ef14340efeb50650fa8ff69de8722d10
C33-star <crusadr33@gmail.com>
2026-07-15 14:04:25 +0800

    Added parts that should connect to search

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const handlebars = req9ore
+const expressHandlebars = require('express-handlebars');
 const path = require('path');
 const { connectToMongo } = require('./private/connection');
+const { createReservation } = require('./private/controllers/reservations');
 const server = express();
 
-// Exposes every file in the public folder
+const handlebars = expressHandlebars.create({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    helpers: {
+        eq: (a, b) => a == b
+    }
+});
+
+server.engine('hbs', handlebars.engine);
+
+server.set('view engine', 'hbs');
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(express.json());
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.render('index', {
+        page: '/',
+        script: '/scripts/index.js'
+    });
 });
 
 server.get('/flight-search', (req, res) => {
@@ -16,11 +35,54 @@ server.get('/flight-search', (req, res) => {
 });
 
 server.get('/flight-book', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/user/booking.html'));
+    res.render('booking', {
+        page: '/flight-book',
+        script: '/scripts/user/booking.js'
+    });
+});
+
+server.get('/api/search', async (req, res) => {
+  const { originAirport, destinationAirport, date } = req.query;
+  
+  // Construct dynamic query
+  let query = {};
+  if (originAirport) query.originAirport = originAirport;
+  if (destinationAirport) query.destinationAirport = destinationAirport;
+  if (date) {
+    // Match the exact day in MongoDB regardless of time
+    const start = new Date(date);
+    start.setUTCHours(0,0,0,0);
+    const end = new Date(date);
+    end.setUTCHours(23,59,59,999);
+    query.date = { $gte: start, $lte: end };
+  }
+
+  try {
+    const results = await Record.find(query);
+    res.json(results);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+server.post('/flight-book', (req, res) => {
+    // if user has not picked flight and goes to this link,
+    // redirect to flight-search
+
+    try {
+        const reservation = createReservation(req.body);
+        res.status(200).json({ success: true, reservation });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Failed To Create Reservation!' });
+    }
 });
 
 server.get('/reservations', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/user/reservations.html'));
+    res.render('reservations', {
+        page: '/reservations',
+        script: '/scripts/user/reservations.js'
+    });
 });
 
 connectToMongo((err) => {
@@ -30,5 +92,7 @@ connectToMongo((err) => {
         server.listen(process.env.SERVER_PORT, () => {
             console.log(`Server Running On http://localhost:${process.env.SERVER_PORT}!`);
         });
+    }
+});
     }
 });
