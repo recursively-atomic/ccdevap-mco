ada64e6c10acc8ca8877084b04775df3620923e7
kyeeona-c <kyeeona_caesar@dlsu.edu.ph>
2026-08-04 19:15:38 +0800

    Refactor server.js to improve structure and functionality

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-require('dotenv').config();
-
-const Users = require('./private/models/users');
 const express = require('express');
-const session = require("express-session");
+const session = require('express-session');
 const expressHandlebars = require('express-handlebars');
+const cors = require('cors');
 const path = require('path');
-const { connectToMongo } = require('./private/connection');
-const {
-    createUser,
-    getUserById,
-    getUserByEmail,
-    getAllUsers,
-    updateUser,
-    changePassword
-} = require('./private/controllers/users');
-
 const server = express();
 
-server.use(session({
-    secret: "airline-secret-key",
-    resave: false,
-    saveUninitialized: false
-}));
+const {
+    eq, gt, gte,
+    lt, lte, add,
+    subtract,
+    range, or, and,
+    formatNumber,
+    formatTitleCase,
+    formatAuditLog,
+    getDate,
+    getSpecificTime,
+    getTime,
+    getDuration,
+    getUserNumber,
+    getAirlineLogo,
+    getFlightNumber
+} = require('./private/helpers');
+
+const { connectToServer } = require('./private/connection');
+const userRoutes = require("./private/routes/userRoutes");
+const flightRoutes = require("./private/routes/flightRoutes");
+const reservationRoutes = require('./private/routes/reservationRoutes');
+const auditRoutes = require("./private/routes/auditRoutes");
 
 const handlebars = expressHandlebars.create({
     extname: 'hbs',
@@ -29,7 +34,20 @@ const handlebars = expressHandlebars.create({
     layoutsDir: path.join(__dirname, 'views', 'layouts'),
     partialsDir: path.join(__dirname, 'views', 'partials'),
     helpers: {
-        eq: (a, b) => a == b
+        eq, gt, gte,
+        lt, lte, add,
+        subtract,
+        range, or, and,
+        formatNumber,
+        formatTitleCase,
+        formatAuditLog,
+        getDate,
+        getSpecificTime,
+        getTime,
+        getDuration,
+        getUserNumber,
+        getAirlineLogo,
+        getFlightNumber
     }
 });
 
@@ -38,219 +56,44 @@ server.engine('hbs', handlebars.engine);
 server.set('view engine', 'hbs');
 server.set('views', path.join(__dirname, 'views'));
 
+server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(express.static(path.join(__dirname, 'public'))); // Exposes every file in the public folder
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.get('/', (req, res) => {
-    res.render('index', {
-        page: '/',
-        script: '/scripts/index.js'
+server.use(session({
+    secret: "airline-secret-key",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// ============================================================================
+// RELOADS THE FRONTEND WHENEVER A CHANGE IS MADE
+if (process.env.NODE_ENV !== 'production') {
+    const livereload = require("livereload");
+    const connectLiveReload = require("connect-livereload");
+    const liveReloadServer = livereload.createServer({
+        exts: ['hbs', 'css', 'js'],
+        exclusions: [/node_modules/]
     });
-});
 
-server.get("/register", (req, res) => {
-    res.render('register', { message: "user registration page" });
-});
-
-server.post("/register", async (req, res) => {
-    try {
-        const user = await createUser(req.body);
-        res.redirect("/login");
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Registration Failed.");
-    }
-});
-
-server.get('/login', (req, res) => {
-    res.render('login', { message: "user login page"});
-});
-
-server.post("/login", async (req, res) => {
-    try {
-        const email = req.body.emailAddress;
-        const password = req.body.password;
-        const user = await Users.findOne({
-            emailAddress: email
-        });
-
-        if (!user) {
-            return res.send("User not found.");
-        }
-
-        if (user.password !== password) {
-            return res.send("Incorrect password.");
-        }
-
-        req.session.user = {
-        _id: user._id,
-        emailAddress: user.emailAddress,
-        role: user.role
-        };
-
-        if (user.role === "admin") {
-            return res.redirect("/admin-profile");
-        }
-        res.redirect("/user-profile");
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Login failed.");
-    }
-});
-
-server.put("/api/users/change-password", async (req, res) => {
-    try {
-        const { currentPassword, newPassword } = req.body;
-        await changePassword(
-            req.session.user._id,
-            currentPassword,
-            newPassword
-        );
-        res.json({
-            success: true,
-            message: "Password updated successfully."
-        });
-
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            message: err.message
-        });
-    }
-});
-
-server.get("/logout", (req, res) => {
-    req.session.destroy(() => {
-        res.redirect("/login");
+    liveReloadServer.watch([__dirname + "/views", __dirname + "/public"]);
+    server.use(connectLiveReload());
+    liveReloadServer.server.once("connection", () => {
+        setTimeout(() => {
+            liveReloadServer.refresh("/");
+        }, 1);
     });
-});
+}
+// ============================================================================
 
-server.put("/api/profile", async (req, res) => {
-    try {
-        if (!req.session.user) {
-            return res.status(401).json({
-                success: false,
-                message: "Not logged in."
-            });
-        }
+server.use("/", userRoutes);
+server.use("/", flightRoutes);
+server.use("/", reservationRoutes);
+server.use("/", auditRoutes);
 
-        const user = await updateUser(
-            req.session.user._id,
-            req.body
-        );
+module.exports = server;
 
-        req.session.user.emailAddress = user.emailAddress;
-        
-        res.json({
-            success: true,
-            user
-        });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            success: false,
-            message: "Unable to update profile."
-        });
-    }
-});
-
-server.get("/user-profile", async (req, res) => {
-    if (!req.session.user) {
-        return res.redirect("/login");
-    }
-    const user = await getUserById(req.session.user._id);
-    res.render("user", {
-        user
-    });
-});
-
-server.get("/admin-profile", async (req, res) => {
-    if (!req.session.user || req.session.user.role !== "admin") {
-        return res.redirect("/login");
-    }
-
-    const user = await getUserById(req.session.user._id);
-
-    res.render("admin", {
-        user
-    });
-});
-
-server.get("/user/:id", async (req, res) => {
-    const user = await getUserById(req.params.id);
-    res.render("user", {
-        user
-    });
-});
-
-server.get("/admin-users", async (req, res) => {
-    try {
-        const users = await getAllUsers();
-        res.render("admin-users", {
-            users,
-            layout: "main"
-        });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Unable to load users.");
-    }
-});
-
-server.get("/api/users", async (req, res) => {
-    try {
-        const users = await getAllUsers();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        });
-    }
-});
-
-server.get("/api/users/:id", async (req, res) => {
-    try {
-        const user = await getUserById(req.params.id);
-        res.json(user);
-
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
-});
-
-server.put("/api/users/:id", async (req, res) => {
-    try {
-        const updatedUser = await updateUser(req.params.id, req.body);
-        res.json({
-            success: true,
-            user: updatedUser
-        });
-
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
-});
-
-server.use((req, res) => {
-    res.status(404).send("Page not found.");
-});
-
-connectToMongo((err) => {
-    if (err) {
-        console.error('Server Not Started!');
-    } else {
-        server.listen(process.env.SERVER_PORT, () => {
-            console.log(`Server Running On http://localhost:${process.env.SERVER_PORT}!`);
-        });
-    }
-});
+if (require.main === module) {
+    connectToServer(server);
+}
